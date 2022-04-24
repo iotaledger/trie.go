f5e23ac0be4522a92bba1d57144336526c7cfb83
lunfardo314 <evaldas.drasutis@iota.org>
2022-04-24 20:27:20 +0300

    trie_bench utility with Badger

--- hive_adaptor/hiveadaptor.go
+++ hive_adaptor/hiveadaptor.go
@@ -7,6 +7,7 @@ import (
 	"github.com/iotaledger/trie.go/trie256p"
 )
 
+// HiveKVStoreAdaptor maps a partition of the Hive KVStore to trie_go.KVStore
 type HiveKVStoreAdaptor struct {
 	kvs    kvstore.KVStore
 	prefix []byte
@@ -28,6 +29,7 @@ func makeKey(prefix, k []byte) []byte {
 	}
 	return trie_go.Concat(prefix, k)
 }
+
 func (kvs *HiveKVStoreAdaptor) Get(key []byte) []byte {
 	v, err := kvs.kvs.Get(makeKey(kvs.prefix, key))
 	if errors.Is(err, kvstore.ErrKeyNotFound) {
@@ -60,64 +62,73 @@ func (kvs *HiveKVStoreAdaptor) Iterate(fun func(k []byte, v []byte) bool) {
 	mustNoErr(err)
 }
 
-type batchPartition struct {
-	prefix []byte
-	batch  kvstore.BatchedMutations
-}
-
-func newBatchPartition(prefix []byte, batch kvstore.BatchedMutations) *batchPartition {
-	return &batchPartition{
-		prefix: prefix,
-		batch:  batch,
-	}
-}
-
-func (k *batchPartition) Set(key, value []byte) {
-	if err := k.batch.Set(makeKey(k.prefix, key), value); err != nil {
-		panic(err)
-	}
-}
-
-func (k *batchPartition) Del(key []byte) {
-	if err := k.batch.Delete(makeKey(k.prefix, key)); err != nil {
-		panic(err)
-	}
-}
-
+// HiveBatchedUpdater implements buffering and flush updates in batches, both k/v pairs and trie.
+// Dramatically improves speed
 type HiveBatchedUpdater struct {
-	kvs             kvstore.KVStore
-	batch           kvstore.BatchedMutations
-	trieBatch       *batchPartition
-	valueStoreBatch *batchPartition
-	trie            *trie256p.Trie
+	kvs              kvstore.KVStore
+	batch            kvstore.BatchedMutations
+	wTrie            batchWriter
+	wValue           batchWriter
+	triePrefix       []byte
+	valueStorePrefix []byte
+	trie             *trie256p.Trie
 }
 
-func NewHiveBatchedUpdater(kvs kvstore.KVStore, trie *trie256p.Trie, triePrefix, storePrefix []byte) (*HiveBatchedUpdater, error) {
-	b, err := kvs.Batched()
-	if err != nil {
-		return nil, err
-	}
+func NewHiveBatchedUpdater(kvs kvstore.KVStore, model trie256p.CommitmentModel, triePrefix, storePrefix []byte) (*HiveBatchedUpdater, error) {
 	ret := &HiveBatchedUpdater{
-		kvs:             kvs,
-		trieBatch:       newBatchPartition(triePrefix, b),
-		valueStoreBatch: newBatchPartition(storePrefix, b),
-		trie:            trie,
-		batch:           b,
+		kvs:              kvs,
+		trie:             trie256p.New(model, NewHiveKVStoreAdaptor(kvs, triePrefix)),
+		triePrefix:       triePrefix,
+		valueStorePrefix: storePrefix,
 	}
 	return ret, nil
 }
 
 func (a *HiveBatchedUpdater) Update(key []byte, value []byte) {
-	if len(value) > 0 {
-		a.valueStoreBatch.Set(key, value)
-	} else {
-		a.valueStoreBatch.Del(key)
+	var err error
+	if a.batch == nil {
+		a.batch, err = a.kvs.Batched()
+		mustNoErr(err)
+		a.wTrie = newBatchWriter(a.batch, a.triePrefix)
+		a.wValue = newBatchWriter(a.batch, a.valueStorePrefix)
 	}
+	a.wValue.Set(key, value)
 	a.trie.Update(key, value)
 }
 
+// batchWriter implements KVWriter interface over the Hive batch
+type batchWriter struct {
+	prefix []byte
+	batch  kvstore.BatchedMutations
+}
+
+func newBatchWriter(b kvstore.BatchedMutations, prefix []byte) batchWriter {
+	return batchWriter{
+		prefix: prefix,
+		batch:  b,
+	}
+}
+
+func (b batchWriter) Set(key, value []byte) {
+	var err error
+	if len(value) > 0 {
+		err = b.batch.Set(makeKey(b.prefix, key), value)
+	} else {
+		err = b.batch.Delete(makeKey(b.prefix, key))
+	}
+	mustNoErr(err)
+}
+
 func (a *HiveBatchedUpdater) Commit() error {
-	a.trie.PersistMutations(a.trieBatch)
+	if a.batch == nil {
+		return nil
+	}
+	a.trie.Commit()
+	a.trie.PersistMutations(a.wTrie)
+	if err := a.batch.Commit(); err != nil {
+		return err
+	}
 	a.trie.ClearCache()
-	return a.batch.Commit()
+	a.batch = nil
+	return nil
 }
--- trie_bench/main.go
+++ trie_bench/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/iotaledger/hive.go/kvstore"
 	"github.com/iotaledger/hive.go/kvstore/badger"
+	"github.com/iotaledger/hive.go/kvstore/mapdb"
 	trie_go "github.com/iotaledger/trie.go"
 	"github.com/iotaledger/trie.go/hive_adaptor"
 	"github.com/iotaledger/trie.go/trie256p"
@@ -17,7 +19,7 @@ const usage = "generate random key/value pairs. USAGE: trie_bench -gen <size> <n
 	"generate random key/value pairs with 32 byte random keys. USAGE: trie_bench -genhash <size> <name>\n" +
 	"make badger DB with trie from file. USAGE: trie_bench -mkdbbadger <name>\n" +
 	"make in-memory DB with trie from file. USAGE: trie_bench -mkdbmem <name>\n" +
-	"check consistency of the DB. USAGE: trie_bench -checkdb <name>\n"
+	"check consistency of the DB. USAGE: trie_bench -checkdbbadger <name>\n"
 
 func main() {
 	if len(os.Args) < 2 {
@@ -57,12 +59,19 @@ func main() {
 		}
 		mkdbmem(os.Args[2])
 
-	case "-checkdb":
+	case "-checkdbbadger":
 		if len(os.Args) != 3 {
 			fmt.Printf(usage)
 			os.Exit(1)
 		}
-		checkdb(os.Args[2])
+		checkdbbadger(os.Args[2])
+
+	case "-scandbbadger":
+		if len(os.Args) != 3 {
+			fmt.Printf(usage)
+			os.Exit(1)
+		}
+		scandbbadger(os.Args[2])
 
 	default:
 		fmt.Printf(usage)
@@ -78,11 +87,11 @@ func must(err error) {
 }
 
 const (
-	MaxKey   = 64
-	MaxValue = 128
+	MaxKey   = 100
+	MaxValue = 32
 )
 
-func genrnd(size int, name string, hashkeys bool) {
+func genrnd(size int, name string, hashKV bool) {
 	rndIterator := trie_go.NewRandStreamIterator(trie_go.RandStreamParams{
 		Seed:       time.Now().UnixNano(),
 		NumKVPairs: size,
@@ -100,9 +109,13 @@ func genrnd(size int, name string, hashkeys bool) {
 		if (count+1)%100000 == 0 {
 			fmt.Printf("writing key/value pair %d. Wrote %d bytes\n", count+1, wrote)
 		}
-		if hashkeys {
+		if hashKV {
 			t := blake2b.Sum256(k)
 			k = t[:]
+			if len(v) > 0 {
+				t = blake2b.Sum256(v)
+				v = t[:]
+			}
 		}
 		must(fileWriter.Write(k, v))
 		count++
@@ -114,40 +127,13 @@ func genrnd(size int, name string, hashkeys bool) {
 }
 
 // all values loads in memory
+const flushEach = 100_000
 
 func mkdbmem(name string) {
-	triePartition := trie_go.NewInMemoryKVStore()
-	kvPartition := trie_go.NewInMemoryKVStore()
-	trie := trie256p.New(trie_blake2b.New(), triePartition)
-
 	fname := name + ".bin"
-	streamIn, err := trie_go.OpenKVStreamFile(fname)
-	must(err)
-	defer streamIn.Close()
-
-	tm := NewTimer()
-	counterRec := 0
-	err = streamIn.Iterate(func(k []byte, v []byte) bool {
-		kvPartition.Set(k, v)
-		trie.Update(k, v)
-		counterRec++
-		return true
-	})
-	trie.Commit()
-	counterMut := trie.PersistMutations(triePartition)
-	trie.ClearCache()
-	must(err)
-
-	fmt.Printf("total records: %d\ntotal trie nodes: %d\n", counterRec, counterMut)
-	fmt.Printf("Speed: %f records/sec\n", float64(counterRec)/tm.Duration().Seconds())
+	kvs := mapdb.NewMapDB()
+	file2kvs(fname, kvs)
 }
-
-var (
-	triePrefix       = []byte{0x01}
-	valueStorePrefix = []byte{0x02}
-)
-
-const flushEach = 100_000
 
 // all value and trie in badger db. Flushes every 100_000 records
 
@@ -163,41 +149,12 @@ func mkdbbadger(name string) {
 	db, err := badger.CreateDB(dbDir)
 	must(err)
 	kvs := badger.New(db)
-	triePartition := hive_adaptor.NewHiveKVStoreAdaptor(kvs, triePrefix)
-	trie := trie256p.New(trie_blake2b.New(), triePartition)
-	var updater *hive_adaptor.HiveBatchedUpdater
 	must(err)
 
-	streamIn, err := trie_go.OpenKVStreamFile(fname)
-	must(err)
-	defer streamIn.Close()
-
-	tm := NewTimer()
-	counterRec := 1
-	err = streamIn.Iterate(func(k []byte, v []byte) bool {
-		if updater == nil {
-			updater, err = hive_adaptor.NewHiveBatchedUpdater(kvs, trie, triePrefix, valueStorePrefix)
-			must(err)
-		}
-		updater.Update(k, v)
-
-		if counterRec%flushEach == 0 {
-			must(updater.Commit())
-			updater = nil
-			fmt.Printf("commited %d records. Duration: %v\n", counterRec, tm.Duration())
-		}
-		counterRec++
-		return true
-	})
-	must(err)
-	if updater != nil {
-		must(updater.Commit())
-		fmt.Printf("commited %d records. Duration: %v\n", counterRec, tm.Duration())
-	}
-	fmt.Printf("Speed: %f records/sec\n", float64(counterRec)/tm.Duration().Seconds())
+	file2kvs(fname, kvs)
 }
 
-func checkdb(name string) {
+func checkdbbadger(name string) {
 	dbDir := name + ".dbdir"
 	if _, err := os.Stat(dbDir); os.IsNotExist(err) {
 		fmt.Printf("directory %s does not exist\n", dbDir)
@@ -229,9 +186,105 @@ func checkdb(name string) {
 	})
 }
 
+func scandbbadger(name string) {
+	dbDir := name + ".dbdir"
+	if _, err := os.Stat(dbDir); os.IsNotExist(err) {
+		fmt.Printf("directory %s does not exist\n", dbDir)
+		os.Exit(1)
+	}
+	fmt.Printf("opening database '%s'\n", dbDir)
+
+	db, err := badger.CreateDB(dbDir)
+	must(err)
+	kvs := badger.New(db)
+	trieKVS := hive_adaptor.NewHiveKVStoreAdaptor(kvs, triePrefix)
+	valueKVS := hive_adaptor.NewHiveKVStoreAdaptor(kvs, valueStorePrefix)
+
+	recCounter := 0
+	keyByteCounter := 0
+	valueKVS.Iterate(func(k []byte, v []byte) bool {
+		recCounter++
+		keyByteCounter += len(k)
+		return true
+	})
+	fmt.Printf("K/V STORAGE: number of key/value pairs: %d, avg key len: %d\n",
+		recCounter, keyByteCounter/recCounter)
+
+	recCounter = 0
+	keyByteCounter = 0
+	valueByteCounter := 0
+	trieKVS.Iterate(func(k []byte, v []byte) bool {
+		recCounter++
+		keyByteCounter += len(k)
+		valueByteCounter += len(v)
+		return true
+	})
+	fmt.Printf("TRIE: number of nodes: %d, avg key len: %d, avg node size: %d\n",
+		recCounter, keyByteCounter/recCounter, valueByteCounter/recCounter)
+
+	model := trie_blake2b.New()
+	trie := trie256p.NewNodeStoreReader(trieKVS, model)
+	root := trie256p.RootCommitment(trie)
+	fmt.Printf("root commitment: %s\n", root)
+
+	recCounter = 1
+	proofBytes := 0
+	proofLen := 0
+	tm := newTimer()
+	valueKVS.Iterate(func(k []byte, v []byte) bool {
+		proof := model.Proof(k, trie)
+		proofBytes += len(proof.Bytes())
+		proofLen += len(proof.Path)
+		err = proof.Validate(root, v)
+		must(err)
+		if recCounter%flushEach == 0 {
+			fmt.Printf("validated %d records in %v, %f proof/sec, avg proof bytes %d, avg proof len %f\n",
+				recCounter, tm.Duration(), float64(recCounter)/tm.Duration().Seconds(),
+				proofBytes/recCounter, float32(proofLen)/float32(recCounter))
+		}
+		recCounter++
+		return true
+	})
+}
+
 type timer time.Time
 
-func NewTimer() timer {
+var (
+	triePrefix       = []byte{0x01}
+	valueStorePrefix = []byte{0x02}
+)
+
+func file2kvs(fname string, kvs kvstore.KVStore) {
+	streamIn, err := trie_go.OpenKVStreamFile(fname)
+	must(err)
+	defer streamIn.Close()
+
+	tm := newTimer()
+	counterRec := 1
+	model := trie_blake2b.New()
+	trie := trie256p.NewNodeStoreReader(hive_adaptor.NewHiveKVStoreAdaptor(kvs, triePrefix), model)
+	updater, err := hive_adaptor.NewHiveBatchedUpdater(kvs, model, triePrefix, valueStorePrefix)
+	must(err)
+	err = streamIn.Iterate(func(k []byte, v []byte) bool {
+		updater.Update(k, v)
+		if counterRec%flushEach == 0 {
+			must(updater.Commit())
+			fmt.Printf("commited %d records. Duration: %v\n", counterRec, tm.Duration())
+		}
+		counterRec++
+		return true
+	})
+	must(err)
+	if updater != nil {
+		must(updater.Commit())
+		fmt.Printf("commited %d records. Duration: %v\n", counterRec, tm.Duration())
+	}
+	fmt.Printf("Speed: %f records/sec\n", float64(counterRec)/tm.Duration().Seconds())
+
+	fmt.Printf("root commitment: %s\n", trie256p.RootCommitment(trie))
+}
+
+func newTimer() timer {
 	return timer(time.Now())
 }
 
