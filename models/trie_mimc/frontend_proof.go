6e815bcc35c25b6c5684ae55d1b612be25b10479
Andreas Penzkofer <36140574+apenzk@users.noreply.github.com>
2022-09-13 12:53:57 +0100

    Update models/trie_mimc/frontend_proof.go

--- models/trie_mimc/frontend_proof.go
+++ models/trie_mimc/frontend_proof.go
@@ -89,7 +89,7 @@ func NBytesLeftShift(api frontend.API, input frontend.Variable, N int) frontend.
 }
 
 // Return the result of left shift by 1 byte, input size = 32 bytes (fixed)
-// Note: The MSB should be 0, or overflow occurs (the overflow behavior is not simply `mod“ by 2^256)
+// Note: The MSB should be 0, or overflow occurs (the overflow behavior is not simply ``mod'' by 2^256)
 // Input: {0, 42, 194, .. X other variables .., 231}
 // Output: {42, 194, , .. X other variables .., 231, 0}
 func leftShift1Byte(api frontend.API, input frontend.Variable) frontend.Variable {
